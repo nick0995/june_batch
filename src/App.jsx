2c59ef663da4c895447d0af2143e93b9234dee0e
nick0995 <neeraj.chauhan.49@gmail.com>
2024-06-21 10:42:27 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentImg from "./assets/components.png";
+
+const reactDescription = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+function CoreConcepts(props) {
+  return (
+    <li>
+      <img src={props.image} alt={props.title}></img>
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function Header() {
+  const description = reactDescription[genRandomInt(2)];
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {description} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
@@ -14,9 +34,33 @@ function Header() {
 function App() {
   return (
     <div>
-      <Header/>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcepts
+              title="Components"
+              description="The core building blocks."
+              image={componentImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The core building blocks."
+              image={componentImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The core building blocks."
+              image={componentImg}
+            />
+            <CoreConcepts
+              title="Components"
+              description="The core building blocks."
+              image={componentImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
