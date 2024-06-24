fd393b4e63d468a4ae6b7e1c3163e6bdb5e2f334
nick0995 <neeraj.chauhan.49@gmail.com>
2024-06-24 18:44:44 +0530

    rendering updated hook value

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import { useState } from "react";
 import Header from "./components/Header/Header";
 import CoreConcepts from "./components/CoreConcepts";
 import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data";
+import { CORE_CONCEPTS, EXAMPLES } from "./data";
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+  const [selectedTopic, setSelectedTopic] = useState("components");
 
   function handleSelect(selectedButton) {
     // Select button --> 'component', jsx, prop, state
     setSelectedTopic(selectedButton);
-    console.log(selectedTopic);
+    // console.log(selectedTopic);
   }
   console.log(" APP COMPONENT");
   console.log("Inside the component", selectedTopic);
@@ -35,13 +35,20 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect("component")}>
+            <TabButton onSelect={() => handleSelect("components")}>
               Component
             </TabButton>
             <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
