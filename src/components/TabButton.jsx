cd89e0cdcead3b4d737d9b8c01a2849b98ee62b6
nick0995 <neeraj.chauhan.49@gmail.com>
2024-06-25 10:48:31 +0530

    initial

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,10 @@
-export default function TabButton({ children, onSelect }) {
+export default function TabButton({ children, onSelect, isSelected }) {
   console.log(" TAB BUTTON COMPONENT");
   return (
     <li>
-      <button onClick={onSelect}>{children}</button>
+      <button className={isSelected ? "active" : undefined} onClick={onSelect}>
+        {children}
+      </button>
     </li>
   );
 }
