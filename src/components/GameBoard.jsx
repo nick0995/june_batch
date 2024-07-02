faee7042dac83f0196b37d4177084effa7e90dc9
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-02 10:54:53 +0530

    initial

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -32,7 +32,9 @@ export default function GameBoard({ onSelectSquare, turns }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled = {playerSymbol !== null}
+                  >
                   {playerSymbol}
                 </button>
               </li>
--- src/components/Log.jsx
+++ src/components/Log.jsx
@@ -1,11 +1,11 @@
 export default function Log({ turns }) {
   return (
     <ol id="log">
-      {turns.map((turn) => {
-        <li key={`${turn.square.row}${turn.square.col}`}>
+      {turns.map((turn) => (
+        <li>
           {turn.player} selected {turn.square.row},{turn.square.col}
-        </li>;
-      })}
+        </li>
+      ))}
     </ol>
   );
 }
