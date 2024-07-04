8b391f8c1e4203706e618f2146c8d760715fa0ee
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-04 17:34:09 +0530

    init

--- src/App.jsx
+++ src/App.jsx
@@ -67,8 +67,13 @@ function App() {
     });
   }
 
+  const ctxValue = {
+    items: shoppingCart.items,
+    addItemToCart: handleAddItemToCart
+  }
+  
   return (
-    <CartContext.Provider value={{ items: [] }}>
+    <CartContext.Provider value={ctxValue}>
       <Header
         cart={shoppingCart}
         onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
@@ -76,7 +81,7 @@ function App() {
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Product {...product} onAddToCart={handleAddItemToCart} />
+            <Product {...product} />
           </li>
         ))}
       </Shop>
--- src/store/shopping-cart-context.jsx
+++ src/store/shopping-cart-context.jsx
@@ -2,4 +2,5 @@ import { createContext } from "react";
 
 export const CartContext = createContext({
   items: [],
+  addItemToCart: () => {},
 });
