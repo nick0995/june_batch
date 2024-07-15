27933e6482e0df2997ed8fb254e18bd8d2c72a8c
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-15 18:25:00 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // createRoutesFromElements,
+  // Route
+} from "react-router-dom";
 
-import HomePage from './pages/Home';
-import ProductsPage from './pages/Product';
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Product";
+
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />}/>
+//     <Route path= "/products" element= {<ProductsPage />}/>
+//   </Route>
+// )
+
+// const router = createBrowserRouter(routeDefinations);
 
 const router = createBrowserRouter([
-  { path: '/', element: < HomePage /> },
-  { path: '/products', element: < ProductsPage/> }
-
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
 ]);
 
 function App() {
