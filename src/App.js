f22692734a35b85be9602e399d8d9b9201cbfde6
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-16 10:21:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import {
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Product";
 import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
 
 // const routeDefinations = createRoutesFromElements(
 //   <Route>
@@ -22,6 +23,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router-dom";
 
 import MainNavigation from "../components/MainNavigation";
-import classes from "./Root.module.css";
 
 function RootLayout() {
     return (
         <>
             <MainNavigation />
-            <main className={classes.content}>
+            <main>
                 <Outlet />
             </main>
         </>
