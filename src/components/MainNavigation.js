af42deeb1a085b2dcd6a0a1bcfd01d2a6426be5b
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-16 17:40:10 +0530

    init

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,22 +1,37 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-import classes from './MainNavigation.module.css';
+import classes from "./MainNavigation.module.css";
 
 function MainNavigation() {
-    return (
-        <header className={classes.header}>
-            <nav>
-                <ul className={classes.list}>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/products">Products</Link>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    )
+  return (
+    <header className={classes.header}>
+      <nav>
+        <ul className={classes.list}>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => 
+                isActive ? classes.active : undefined
+              }
+              end
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/products"
+              className={({ isActive }) => 
+                isActive ? classes.active : undefined
+              }
+            >
+              Products
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default MainNavigation;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate('/products')
+  }
   return (
     <>
       <h1>My Home Page!!</h1>
       <p>
         Go to <Link to="/products">the list of products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
