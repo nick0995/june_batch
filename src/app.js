64ce83fda7c50e46a023fe23058fda109d64de46
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-19 18:35:54 +0530

    added public folder

--- src/app.js
+++ src/app.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const path = require("path");
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname,"../public");
 
-app.get("/", (req, res) => {
-  res.send("Weclome to my application!");
-});
+console.log(path.join(__dirname,"../public"));
+app.use(express.static(publicDirectoryPath));
 
 app.get("/about", (req, res) => {
-  res.send("about");
+  res.send("<h1>about<h1>");
 });
 
 app.get("/help", (req, res) => {
   res.send("help");
+});
+
+app.get("weather", (req,res)=>{
+    res.send("weather");
 });
 
 app.listen(3000, () => {
