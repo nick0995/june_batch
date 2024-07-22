446e10ca36c59226044b3daa48e9b8cb4dfd13f9
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-22 18:37:43 +0530

    improvement in routing and added partials for hbs template

--- src/app.js
+++ src/app.js
@@ -1,37 +1,57 @@
 const express = require("express");
 const path = require("path");
+const hbs = require("hbs");
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname,"../public");
 
+// Define paths for Express config
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const publicDirectoryPath = path.join(__dirname, "../public");
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup static directory to serve
 app.use(express.static(publicDirectoryPath));
 
 app.get("", (req, res) => {
   res.render("index", {
     title: "Weather app",
-    author: "Neeraj Chauhan",
+    name: "Neeraj Chauhan",
   });
 });
 
 app.get("/help", (req, res) => {
   res.render("help", {
+    helpText: "This is some helpful text.",
     title: "Help",
-    author: "Neeraj Chauhan",
+    name: "Neeraj Chauhan",
   });
 });
 
 app.get("/about", (req, res) => {
   res.render("about", {
-    title: "Help",
-    author: "Neeraj Chauhan",
+    title: "About me",
+    name: "Neeraj Chauhan",
   });
 });
+
 app.get("/weather", (req,res)=>{
     res.send({
       forecast: 'It is snowing!',
       location: "Shimla"
     });
+});
+
+app.get("*", (req, res) => {
+  res.render('404', {
+    title: '404',
+    name: 'Neeraj Chauhan',
+    errorMessage: 'Page not found!'
+  })
 });
 
 app.listen(3000, () => {
