a44ed5a433532039299578b68b2feb618c22b216
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-22 18:00:11 +0530

    view added

--- src/app.js
+++ src/app.js
@@ -4,8 +4,29 @@ const path = require("path");
 const app = express();
 const publicDirectoryPath = path.join(__dirname,"../public");
 
+app.set("view engine", "hbs");
 app.use(express.static(publicDirectoryPath));
 
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather app",
+    author: "Neeraj Chauhan",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    author: "Neeraj Chauhan",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "Help",
+    author: "Neeraj Chauhan",
+  });
+});
 app.get("/weather", (req,res)=>{
     res.send({
       forecast: 'It is snowing!',
