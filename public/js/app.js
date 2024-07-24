8da151cccbab2d89f61b99fb1a8f0f6aaf12b23d
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-24 18:18:00 +0530

    added api call from fe to bE

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log('This is my static js folder/file');
+
+fetch("http://localhost:3000/weather?address=chandigarh").then((response) => {
+  response.json().then((data) => {
+    if (data.error) {
+      console.log(data.error);
+    } else {
+      console.log(data.location);
+      console.log(data.forecast);
+    }
+  });
+});
--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const hbs = require("hbs");
 
+const geocode = require("./utils/gecode");
+const forecast = require("./utils/forecast");
+
 const app = express();
 
 // Define paths for Express config
@@ -39,11 +42,39 @@ app.get("/about", (req, res) => {
   });
 });
 
-app.get("/weather", (req,res)=>{
-    res.send({
-      forecast: 'It is snowing!',
-      location: "Shimla"
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide an address",
     });
+  }
+
+  geocode(req.query.address, (error, { latitude, longitude, location }) => {
+    if (error) {
+      return res.send(error);
+    }
+    forecast(latitude, longitude, (error, forecasteData) => {
+      if (error) {
+        return res.send(error);
+      }
+
+      return res.send({
+        forecast: forecasteData,
+        location,
+        address: req.query.address,
+      });
+    });
+  });
+});
+
+app.get("/products", (req, res) => {
+  console.log(req.query);
+  if (!req.query.search) {
+    return res.send("Please add one parameter");
+  }
+  res.send({
+    products: [],
+  });
 });
 
 app.get("*", (req, res) => {
