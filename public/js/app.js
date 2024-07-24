f1e833e6bd39c3856596229314193aefc74f2eae
nick0995 <neeraj.chauhan.49@gmail.com>
2024-07-24 18:54:34 +0530

    final project

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,26 @@
-console.log('This is my static js folder/file');
+const weatherForm = document.querySelector("form");
+const search = document.querySelector("input");
+const messageOne = document.querySelector("#message-1");
+const messageTwo = document.querySelector("#message-2");
 
-fetch("http://localhost:3000/weather?address=chandigarh").then((response) => {
-  response.json().then((data) => {
-    if (data.error) {
-      console.log(data.error);
-    } else {
-      console.log(data.location);
-      console.log(data.forecast);
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const location = search.value;
+
+  messageOne.textContent = "Loading...";
+  messageTwo.textContent = "";
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
     }
-  });
+  );
 });
